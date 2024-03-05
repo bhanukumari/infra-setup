0b833c3d179115d3996f8ed3a03dc1540cf35115
bhanukumari <117707378+bhanukumari@users.noreply.github.com>
2024-03-05 16:44:46 +0530

    Update main.tf

---
+++ New folder/ENV/privateroutetable/main.tf
@@ -0,0 +1,13 @@
+resource "aws_route_table" "ngw-rt" {
+  vpc_id = var.vpc_id
+
+  tags = {
+    Name = "nat-gw-rt"
+  }
+}
+resource "aws_route" "r2" {
+  route_table_id         = aws_route_table.ngw-rt.id
+  destination_cidr_block = var.rt_cidr
+  gateway_id             = var.natgate_id
+  
+}
