ced0c108176e37859af7fb3f7100e2d28d566274
bhanukumari <117707378+bhanukumari@users.noreply.github.com>
2024-03-05 16:46:48 +0530

    Update main.tf

---
+++ New folder/ENV/natgateway/main.tf
@@ -0,0 +1,12 @@
+resource "aws_eip" "elasticip" {
+  domain = "vpc"
+}
+
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_eip.elasticip.id
+  subnet_id     = var.sub_id
+
+  tags = {
+    Name = "ninja-nat-01"
+  }
+}
